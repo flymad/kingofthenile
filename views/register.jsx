29f9f2a3d59bf7f09d96d7ed2969c2439ff6a36f
yaztaleb <yaztaleb@gmail.com>
2022-05-24 12:07:12 -0500

    app structur and views

---
+++ views/register.jsx
@@ -0,0 +1,13 @@
+const React = require ("react")
+const Layout = require("./layout/default-layout")
+
+class layout extends React.Component{
+    render(){
+        return(
+            <Layout>
+
+            </Layout>
+        )
+    }
+    
+    }
