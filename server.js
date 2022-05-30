4ef1f575dfce4e9c093852df1500a0946c034fba
yaztaleb <yaztaleb@gmail.com>
2022-05-30 07:53:30 -0500

    added comments to server.js

--- server.js
+++ server.js
@@ -4,20 +4,20 @@ const app = express();
 const port = 2700;
 const mongoose = require("mongoose")
 const Product = require("./model/model")
-//MongoDB connection
 
+// ===== MongoDB connection =====
 mongoose.connect(process.env.MONGO_URI,{
     useNewURLParser:true,
     useUnifiedTopology:true
 })
-mongoose.connection.once("open",() => console.log("Tuan is the man"))
+mongoose.connection.once("open", () => console.log("Connected to Mongo"))
 
-// middleware 
-
+// ===== Middleware ===== 
 app.use(express.urlencoded( {extended:false}))
 app.use(express.static("pictures"))
-app.use(express.json())
 
+// Use Express middleware to parse JSON.
+app.use(express.json())
 // app.use((req, res, next) =>{
 //     console.log("Code working Fine")
 //         next();
@@ -27,20 +27,33 @@ app.use(express.json())
 app.set("view engine","jsx")
 app.engine('jsx', require('express-react-views').createEngine()
 );
-//I.N.D.U.C.E.S
-//index
-app.get("/shop", (req,res)=>{
+
+// ===== Routes =====
+
+// I.N.D.U.C.E.S
+// index, New, Delete, Update, Create, Edit, Show
+
+// Index
+app.get("/shop", (req, res)=> {
+    // Query model to return all products
     Product.find({},(error,foundProduct)=> {
         res.render('Shop',{product:foundProduct})
-    })
-    
+    })  
 })
+
 //NEW
-app.get('/register', (req,res) => {
-    res.render('register')
-})
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+
 //DELETE
+
+
+
 //UPDATE
+
+
+
 //CREATE
 app.post("/shop", (req,res) => {
     console.log(req.body)
@@ -48,17 +61,23 @@ app.post("/shop", (req,res) => {
         res.redirect("/shop")
     })
 })
+
+
+
 //EDIT
+
+
 //SHOW
+
+
 //just reading the attributes of views
 //registeration page view
 
-let tuan =""
 
 
 
 //calling the browser port here
-app.listen(port, ()=> console.log(`listening the port ${port}`))
+app.listen(port, () => console.log(`listening the port ${port}`))
 
 
 
